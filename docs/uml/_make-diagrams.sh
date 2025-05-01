5b16898af2f3f11bee219e12e58dc0989cb0eb88
Vagner Nogueira <vagner.nogueira@gmail.com>
2025-05-01 15:56:15 -0300

    Automated generation of class diagrams

---
+++ docs/uml/_make-diagrams.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if ! command -v plantuml &> /dev/null
+then
+    echo "Error: 'plantuml' not found."
+    exit 1
+fi
+
+SOURCE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ ! -d "$SOURCE_DIR" ]; then
+  echo "Error: Directory '$SOURCE_DIR' not found ou invalid."
+  exit 1
+fi
+
+find "$SOURCE_DIR" -maxdepth 1 -type f -name "*.puml" -print0 | while IFS= read -r -d $'\0' puml_file; do
+  if [ -f "$puml_file" ]; then
+    echo "Process file: '$puml_file'"
+    # Por padrão, o PlantUML gera um arquivo PNG no mesmo diretório do arquivo .puml.
+    # Flags adicionais podem ser usadas:
+    # -tsvg : para gerar SVG em vez de PNG
+    # -o /caminho/destino : para especificar um diretório de saída diferente
+    plantuml "$puml_file"
+
+    if [ $? -eq 0 ]; then
+      echo "  -> Image generated successfully."
+    else
+      echo "  -> Error generating image for '$puml_file'. Check file syntax or PlantUML output."
+    fi
+  fi
+done
+
+echo "Done!"
+
+exit 0
